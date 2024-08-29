9ff3a99256125542ec4c5cdc7ccd12ece79c3939
Giovana Napoli <giovana.ap.napoli@outlook.com>
2024-08-29 17:14:00 -0300

    feat: create form to add thoughts

---
+++ src/app/components/thoughts/create-thoughts/create-thoughts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thoughts',
+  templateUrl: './create-thoughts.component.html',
+  styleUrl: './create-thoughts.component.css'
+})
+export class CreateThoughtsComponent {
+
+}
